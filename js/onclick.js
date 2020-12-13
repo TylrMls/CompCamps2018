b25a8803c89a0a3f5d9e9b7ce86d74aa3673ec89
TylrMls <tylrmls@gmail.com>
2020-12-13 11:50:08 -0500

    Update onclick.js

--- js/onclick.js
+++ js/onclick.js
@@ -1,18 +1,22 @@
 var $elem = $('#toggleButton');
-
+var nav = document.querySelector('.nav');
 function AnimateRotate(angle) {
-  console.log("add");
-  $({deg: 0}).animate({deg: angle}, {
-      duration: 500,
-      step: function(now) {
-          $elem.css({
-              transform: 'rotate(' + now + 'deg)'
-          });
-      }
-  });
-  $('.nav').animate({"margin-left": '+=100%'});
-  document.getElementById("navigation").style.marginLeft = "0";
-  $elem.css({
-      transform: 'rotate(45deg)'
-  });
+  var left_margin = window.getComputedStyle(nav).getPropertyValue("margin-left");
+  if (left_margin == 100%) {
+    console.log("add");
+   $({deg: 0}).animate({deg: angle}, {
+       duration: 500,
+        step: function(now) {
+           $elem.css({
+               transform: 'rotate(' + now + 'deg)'
+            });
+       }
+    });
+  } else {
+   $('.nav').animate({"margin-left": '+=100%'});
+   document.getElementById("navigation").style.marginLeft = "0";
+   $elem.css({
+       transform: 'rotate(45deg)'
+   });
+  }
 }
